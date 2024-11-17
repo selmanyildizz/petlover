c0cfeb246bf778f0128a080b40b17391abe930da
Selman Yıldız <selmanyildiz555@gmail.com>
2024-11-17 17:00:38 +0300

    added country js

---
+++ src/pages/Country.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "../Carousel.css";
+import { div } from "framer-motion/client";
+
+const Country = () => {
+    return (
+        <div>
+            <div className="topMenu">
+
+            </div>
+            <div className="searchBar">
+
+            </div>
+        </div>
+    );
+};
+
+export default Country;
